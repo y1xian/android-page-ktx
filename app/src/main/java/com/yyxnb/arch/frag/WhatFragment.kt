5b53d890f6f6dbebab6c38cd63b89de031ef49ec
yyx <yyxnb@outlook.com>
2019-06-05 18:11:54 +0800

    2.0.0.1 迁移x

---
+++ app/src/main/java/com/yyxnb/arch/frag/WhatFragment.kt
@@ -0,0 +1,24 @@
+package com.yyxnb.arch.frag
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.yyxnb.arch.R
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class WhatFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_what, container, false)
+    }
+
+
+}
